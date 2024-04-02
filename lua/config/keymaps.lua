a34021fa763592910fdef3a47a14aaca7e233b6e
oyanay <oyanay@outbrain.com>
2024-04-03 00:48:28 +0300

    fix which key

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,16 @@
+local keymap = vim.keymap
+local opts = {noremap = true, silent = true}
+-- Directory Navigation
+keymap.set("n", "<leader>e", ":NvimTreeFocus<CR>", opts)
+
+
+-- Pane navigation
+keymap.set("n", "<C-h>", "<C-w>h", opts)
+keymap.set("n", "<C-j>", "<C-w>j", opts)
+keymap.set("n", "<C-k>", "<C-w>k", opts)
+keymap.set("n", "<C-l>", "<C-w>l", opts)
+
+-- Window management
+keymap.set("n", "<leader>wv", ":vsplit<CR>", opts) -- Split vertically
+keymap.set("n", "<leader>wh", ":split<CR>", opts) -- Split horizontally
+keymap.set("n", "<leader>wm", ":MaximizerToggle<CR>", opts) -- Toggle minimize
---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  keys = {
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+      },
+    },
+  config = function()
+    require("nvim-tree").setup {}
+  end,
+}
