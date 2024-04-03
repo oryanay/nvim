2344e456c88e59a4a0cc845d7d121768e9cc2c54
oyanay <oyanay@outbrain.com>
2024-04-03 16:43:07 +0300

    add clipboard key bindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,3 +17,16 @@ keymap.set("n", "<leader>wm", ":MaximizerToggle<CR>", opts) -- Toggle minimize
 -- Exit terminal mode
 keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- Move line in visual mode
+keymap.set("v", "J", ":m '>+1<CR>gv=gv") 
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Copy to clipboard
+keymap.set("x", "<leader>p", "\"_dP") 
+keymap.set("n", "<leader>y", "\"+y") 
+keymap.set("v", "<leader>y", "\"+y")
+keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Delete skip buffer
+keymap.set("n", "<leader>d", "\"_d") 
+keymap.set("v", "<leader>d", "\"_d")
